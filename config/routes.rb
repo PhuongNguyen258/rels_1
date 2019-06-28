834fc9c9781b8ec107ddfd26db879e129413c316
PhuongNguyen258 <n.phuong25082gmail.com>
2019-06-28 10:59:11 +0700

    subject_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  resources :subjects, only: [:index, :show]
   resources :users, except: :destroy
   resources :admin, only: [:index]
   namespace :admin do
